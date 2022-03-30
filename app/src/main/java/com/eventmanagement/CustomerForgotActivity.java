d996990ca5729728fc8ce15007f217728eeb8ece
agkr96 <31437241+agkr96@users.noreply.github.com>
2022-03-30 16:31:52 -0400

    customerForgetActivity

---
+++ app/src/main/java/com/eventmanagement/CustomerForgotActivity.java
@@ -0,0 +1,14 @@
+package com.eventmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerForgotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_forgot);
+    }
+}
